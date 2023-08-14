bab4a29c6ab90b65a931185e5fd837ef814924d6
Thanushree Sridhara.py <66080873+thanubharadwaj@users.noreply.github.com>
2023-08-14 18:35:15 +0200

    Update train.py

--- src/model/train.py
+++ src/model/train.py
@@ -35,7 +35,15 @@ def get_csvs_df(path):
 
 
 # TO DO: add function to split data
+def split_data(df):
+    X = df.drop('Y', axis=1).values
+    y = df['Y'].values
 
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=0)
+    data = {"train": {"X": X_train, "y": y_train},
+            "test": {"X": X_test, "y": y_test}}
+    return data
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # train model
